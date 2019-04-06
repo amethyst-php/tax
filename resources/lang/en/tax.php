84bfcf692ef71d06045c68025eab1408382f32e9
railken <railken@protonmail.ch>
2019-04-06 22:43:04 +0200

    add lang

---
+++ resources/lang/en/tax.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Tax',
+    'description' => 'Tax system, used in provisioning of products or services, and in the invoice system',
+    'attributes' => [
+        'calculator' => [
+            'label' => 'Calculator',
+            'description' => 'Mathematical expression used to calculate the tax'
+        ]
+    ]
+]
---
+++ resources/lang/it/tax.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Tassa',
+    'description' => 'Tassa',
+    'attributes' => [
+        'calculator' => [
+            'label' => 'Calcolatore',
+            'description' => 'Espressione matematica usata per calcolare la tassa'
+        ]
+    ]
+]
